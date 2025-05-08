347e82ab47a56c0c9d759099f3ee3e6506db514a
eduardo <eduardocardoso1039@gmail.com>
2025-05-08 10:11:22 -0300

    Feat | App - Setup app component to show initi modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ModalNewEntityComponent } from "./components/modal-new-entity/modal-new-entity.component";
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
     selector: 'meet-root',
-    imports: [RouterOutlet],
+    imports: [],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'meetime-test';
-}
+  private dialog = inject(MatDialog);
+
+  ngOnInit() {
+    this.dialog.open(ModalNewEntityComponent, {
+      disableClose: true,
+      hasBackdrop: true,  
+      maxWidth: '50vw'
+    });
+  }}
+
