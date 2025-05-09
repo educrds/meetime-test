cde903afb0e1b38156081395b573341bdf61ddde
eduardo <eduardocardoso1039@gmail.com>
2025-05-09 20:55:58 -0300

    Feat | CyclesState - Created state service to store cycles selected

---
+++ src/app/services/cycles-state.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal } from '@angular/core';
+import { Cycle, Structure } from '../models/cycle.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CyclesStateService {
+  /**
+   * A reactive signal that holds the current value of the cycles.
+   * Initialized with a default value of null.
+   */
+  readonly cyclesValue = signal<Structure[]>([]);
+
+  /**
+   * Updates the value of the cycles signal with a new value.
+   * 
+   * @param newValue The new value to set for the entity.
+   */
+  public updateCycleValue(newValue: Structure[]): void {
+    this.cyclesValue.set(newValue);
+  }
+}
