eb6aad20c76a6371ee464b40cf8e25c46a953e06
eduardo <eduardocardoso1039@gmail.com>
2025-05-10 16:26:11 -0300

    Refactor | EntityState - Changed method name

--- src/app/services/entity-state.service.ts
+++ src/app/services/entity-state.service.ts
@@ -8,14 +8,14 @@ export class EntityStateService {
    * A reactive signal that holds the current value of the entity.
    * Initialized with a default value of 1.
    */
-  readonly entityValue = signal(1);
+  readonly entityValue = signal<number>(1);
 
   /**
    * Updates the value of the entity signal with a new value.
    * 
    * @param newValue The new value to set for the entity.
    */
-  public updateEntityValue(newValue: number): void {
+  public updateStateEntityValue(newValue: number): void {
     this.entityValue.set(newValue);
   }
 }
